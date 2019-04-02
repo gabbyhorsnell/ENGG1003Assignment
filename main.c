177309d648e0f495a63c36de0c39b6933065a899
Gabrielle Horsnell <gabbyrjh14@gmail.com>
2019-04-02 22:58:49 +0000

    Began floe control and program setup

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("welcome to the program\n");
+    printf("To use the rotation sipher please enter 1 \n");
+    printf(" To use the enigma sipher please enter 2 \n ");
+    int x=1;
+    scanf( )
+    
+    switch(x){
+        case 1:
+           // function for running the rotation cipher in here 
+        break;
+        case 2:
+            // function for running the enigma cipher 
+        break;
+        default:
+         // make it go back to the start 
+        }
+  
+    
+        
+    
+}

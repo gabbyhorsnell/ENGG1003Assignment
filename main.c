8a27721bbd00d3d39cb595f1e53501b63755fa5c
Gabrielle Horsnell <gabbyrjh14@gmail.com>
2019-04-10 00:25:48 +0000

    added the caesar encryption cipher and  adjusted the program setup

--- main.c
+++ main.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("welcome to the program\n");
-    printf("To use the rotation sipher please enter 1 \n");
-    printf(" To use the enigma sipher please enter 2 \n ");
+
+void caesarCipherEncrypt(void);
+void caesarCipherDecrypt(void);
+void enigmaCipherEncrypt(void);
+void enigmaCipherDecrypt(void);
+
+int main(void){
+    printf("welcome to the encription decription program\n");
+    printf("To use the caesar cipher encription please enter 1 \n");
+    printf("To use the ceaser cipher decription please enter2 \n ");
+    printf("To use the enigma cipher encription please enter 3\n");
+    printf("To use the enigma cipher decription please enter 4 \n ");
+    // Scanf will be inserted here after the whole code is finnished
     int x=1;
-    scanf( )
-    
-    switch(x){
-        case 1:
-           // function for running the rotation cipher in here 
-        break;
-        case 2:
-            // function for running the enigma cipher 
-        break;
-        default:
-         // make it go back to the start 
+ 
+    do{
+        switch(x){
+            case 1:
+             caesarCipherEncrypt();  // function for running the caesar cipher encription 
+            break;
+            case 2:
+              //caesarCipherDecrypt(); // function for running the caesar cipher decription  
+            break;
+            case 3:
+             //enigmaCipherEncrypt(); //function for running the enigma cipher encription
+            break;
+            case 4:
+                //enigmaCipherDecrypt();  //function for running the enigma cipher decription 
+            break;
+            default:
+            printf("invalid imput\n");
         }
-  
+    }while(x>4);
+} 
+// function definition for caesar encription
+void caesarCipherEncrypt(void){
+    char msg[] = {"hello world"};
+    //msg = {"hello world"};
+    int rotation = 7 ;
+    for(int n=0; n<12; n++){
+        if(msg[n] != 32){  
+            msg[n] = msg[n] - 97; // translating the ascii table
+            msg[n] = msg[n] + rotation; // moving the letters
+            msg[n] = msg[n] % 26; // accounting for the overflow i.e. z needs to move to the start of rotation
+            msg[n] = msg[n] + 97; // moving it back into the alaphabet 
+        }
+
+    }
+    printf("%s\n", msg);
     
-        
+}
+// function definiton for caesar decription 
+void caesarCipherDecrypt(void){
     
 }

260e3f14bdcbeb815f2cd491e8053bf17aa8e14a
gabbyhorsnell <48716182+gabbyhorsnell@users.noreply.github.com>
2019-04-29 13:46:16 +1000

    Fixed indenting

--- main.c
+++ main.c
@@ -19,16 +19,16 @@ int main(void){
          scanf("%d", &x); // Allows user to put in their selection
         switch(x){
             case 1:
-             caesarCipherEncrypt();  // Function for running the caesar cipher encription 
+                caesarCipherEncrypt();  // Function for running the caesar cipher encription 
             break;
             case 2:
-              caesarCipherDecrypt(); // Function for running the caesar cipher decription  
+                caesarCipherDecrypt(); // Function for running the caesar cipher decription  
             break;
             case 3:
-             enigmaCipherEncrypt(); //Function for running the enigma cipher encription
+                enigmaCipherEncrypt(); //Function for running the enigma cipher encription
             break;
             case 4:
-             enigmaCipherDecrypt();  //Function for running the enigma cipher decription 
+                enigmaCipherDecrypt();  //Function for running the enigma cipher decription 
             break;
             default:
             printf("invalid imput\n");
@@ -53,7 +53,7 @@ void caesarCipherEncrypt(void){
         }
 
     }
-     printf("%s\n", msg);
+    printf("%s\n", msg);
     
 }
 // Function definiton for caesar decription 
@@ -72,18 +72,18 @@ void caesarCipherDecrypt(void){
             msg[n] = msg[n] + 64; // Moving it back into the alaphabet 
         }
     
-  }
-  printf("%s\n", msg);
+    }
+    printf("%s\n", msg);
 }
  // Function definition for Enigma Encryption
- void enigmaCipherEncrypt(void){
-     char l = 0 ;
-     char msg[1024]; 
-     printf("please enter a message to encrypt\n");
-     scanf(" %[0-9a-zA-Z ]", msg);// to imput meassage that can contain spaces
-     char rotation[26];
-     printf("please enter 26 upercase letters/n");
-     scanf("%26s" , rotation);
+void enigmaCipherEncrypt(void){
+    char l = 0 ;
+    char msg[1024]; 
+    printf("please enter a message to encrypt\n");
+    scanf(" %[0-9a-zA-Z ]", msg);// to imput meassage that can contain spaces
+    char rotation[26];
+    printf("please enter 26 upercase letters/n");
+    scanf("%26s" , rotation);
     /* char rotation[]={20, 14, 7, 24, 4, 6, 18, 1, 12, 7, 14, 24, 3, 15, 17, 19, 24, 23, 22, 25, 25, 16, 5, 8, 9, 3};
      for( int n=0 ; n < 26 ; n++){
            rotation[n] = rotation[n] + 64 ;        
@@ -99,36 +99,36 @@ void caesarCipherDecrypt(void){
         
     }
     for(int n = 0; n < 12; n++){
-    printf("%c",  msg[n]);
-}
+        printf("%c",  msg[n]);
+    }
 
- }
+}
   
  
  // Function  definition for the decription of Enigma Cipher
 void enigmaCipherDecrypt(void){
-     char msg[1024];
-     printf("please enter a message to decrypt\n");
-     scanf(" %[0-9a-zA-Z ]", msg); // to imput a meassage that can contain spaces
+    char msg[1024];
+    printf("please enter a message to decrypt\n");
+    scanf(" %[0-9a-zA-Z ]", msg); // to imput a meassage that can contain spaces
     char rotation[26];
-     printf("please enter 26 upercase letters/n");
-     scanf("%26s" , rotation);
+    printf("please enter 26 upercase letters/n");
+    scanf("%26s" , rotation);
     /* char rotation[]={20, 14, 7, 24, 4, 6, 18, 1, 12, 7, 14, 24, 3, 15, 17, 19, 24, 23, 22, 25, 25, 16, 5, 8, 9, 3};
      for( int n=0 ; n < 26 ; n++){
            rotation[n] = rotation[n] + 64 ;        
         }*/
        for(int n=0 ; n < 1024 ; n++) {
-          if(msg[n] > 64 && msg[n] < 91) { //Using only upercase letters 
-             for(int m = 0; m < 26; m++){ 
-                 if(msg[n] == rotation[m]){
-                     msg[n] = (m + 64); 
-                     break; // to prevent the computer from overflowing 
+            if(msg[n] > 64 && msg[n] < 91) { //Using only upercase letters 
+                for(int m = 0; m < 26; m++){ 
+                    if(msg[n] == rotation[m]){
+                        msg[n] = (m + 64); 
+                        break; // to prevent the computer from overflowing 
                 }
            }
         }
       
     }
-  printf("%s", msg);
+    printf("%s", msg);
 }
 
     
